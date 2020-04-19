8119dedf61e1189e491b2eb81992c2091d9ba944
Julia Schaeffer <julia.schaeffer.berlin@gmail.com>
2020-04-19 13:24:14 +0200

    Add docstring for program restart

--- bikeshare.py
+++ bikeshare.py
@@ -248,7 +248,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+        # ask user whether to restart the program
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
