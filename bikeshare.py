99f778618214ab9cfb38226c9e0e6c8e3eb2cf6c
Julia Schaeffer <julia.schaeffer.berlin@gmail.com>
2020-04-19 14:10:32 +0200

    Refactor most common birth year

--- bikeshare.py
+++ bikeshare.py
@@ -231,7 +231,7 @@ def user_stats(df):
         birth_latest = df['Birth Year'].max()
         print('Most recent year of birth: {}'.format(int(birth_latest)))
         popular_birth_year = df['Birth Year'].mode()[0]
-        print('Most common year of birth:', int(popular_birth_year))
+        print('Most common year of birth: {}'.format(int(popular_birth_year)))
     else:
         print('\nNo data about gender and birth year available!')
 
